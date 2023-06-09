66208f258fda3de1940271813102c5a9958b2789
JadKHaddad <jadkhaddad@gmail.com>
2023-06-09 12:16:06 +0200

    update

--- OpenCL/Gauss/main.cpp
+++ OpenCL/Gauss/main.cpp
@@ -3,6 +3,44 @@
 #include <cmath>
 #include <iomanip>
 #include <stdlib.h>
+#include <CL/cl.h>
+#include <string.h>
+
+cl_platform_id platform;
+cl_device_id device;
+cl_context context;
+cl_command_queue commandQueue;
+cl_kernel kernel;
+
+const char *kernelSource = "__kernel \
+void gaussFilter(__global unsigned char * flatRValues, \
+                    __global unsigned char * flatGValues, \
+                    __global unsigned char * flatBValues, \
+                    __global unsigned char * flatRValuesOut, \
+                    __global unsigned char * flatGValuesOut, \
+                    __global unsigned char * flatBValuesOut, \
+                    __global float * flatWeights, \
+                    const int width, \
+                    const int height) \
+    { \
+        int x = get_global_id(0); \
+        int y = get_global_id(1); \
+        float r = 0.0; \
+        float g = 0.0; \
+        float b = 0.0; \
+        for (int i = -2; i <= 2; i++) \
+        { \
+            for (int j = -2; j <= 2; j++) \
+            { \
+                r += flatRValues[(x + i) + (y + j) * width] * flatWeights[(i + 2) + (j + 2) * 5]; \
+                g += flatGValues[(x + i) + (y + j) * width] * flatWeights[(i + 2) + (j + 2) * 5]; \
+                b += flatBValues[(x + i) + (y + j) * width] * flatWeights[(i + 2) + (j + 2) * 5]; \
+            } \
+        } \
+        flatRValuesOut[x + y * width] = r; \
+        flatGValuesOut[x + y * width] = g; \
+        flatBValuesOut[x + y * width] = b; \
+    }";
 
 typedef struct
 {
@@ -10,6 +48,48 @@ typedef struct
     unsigned char g;
     unsigned char b;
 } Pixel;
+
+unsigned char *flattenRValues(Pixel *image, int width, int height)
+{
+    unsigned char *rValues = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
+    for (int i = 0; i < width * height; i++)
+    {
+        rValues[i] = image[i].r;
+    }
+    return rValues;
+}
+
+unsigned char *flattenGValues(Pixel *image, int width, int height)
+{
+    unsigned char *gValues = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
+    for (int i = 0; i < width * height; i++)
+    {
+        gValues[i] = image[i].g;
+    }
+    return gValues;
+}
+
+unsigned char *flattenBValues(Pixel *image, int width, int height)
+{
+    unsigned char *bValues = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
+    for (int i = 0; i < width * height; i++)
+    {
+        bValues[i] = image[i].b;
+    }
+    return bValues;
+}
+
+Pixel *unflattenRGBValues(unsigned char *rValues, unsigned char *gValues, unsigned char *bValues, int width, int height)
+{
+    Pixel *image = (Pixel *)malloc(sizeof(Pixel) * width * height);
+    for (int i = 0; i < width * height; i++)
+    {
+        image[i].r = rValues[i];
+        image[i].g = gValues[i];
+        image[i].b = bValues[i];
+    }
+    return image;
+}
 
 void printImage(Pixel *image, int width, int height)
 {
@@ -20,6 +100,27 @@ void printImage(Pixel *image, int width, int height)
             std::cout << "(" << (int)image[x + y * width].r << ", " << (int)image[x + y * width].g << ", " << (int)image[x + y * width].b << ") ";
         }
         std::cout << std::endl;
+    }
+}
+
+void compareImages(Pixel *image1, Pixel *image2, int width, int height, const float delta)
+{
+    bool equal = true;
+    for (int i = 0; i < width * height; i++)
+    {
+        if (fabs(image1[i].r - image2[i].r) >= delta || fabs(image1[i].g - image2[i].g) >= delta || fabs(image1[i].b - image2[i].b) >= delta)
+        {
+            equal = false;
+            break;
+        }
+    }
+    if (equal)
+    {
+        std::cout << "Images are equal, delta: " << delta << std::endl;
+    }
+    else
+    {
+        std::cout << "Images are not equal, delta: " << delta << std::endl;
     }
 }
 
@@ -112,6 +213,24 @@ void printWeights(float weights[5][5])
     }
 }
 
+float *flattenWeights(float weights[5][5])
+{
+    float *flattenedWeights = (float *)malloc(sizeof(float) * 25);
+    for (int i = 0; i < 25; i++)
+    {
+        flattenedWeights[i] = weights[i / 5][i % 5];
+    }
+    return flattenedWeights;
+}
+
+void unflattenWeights(float *flattenedWeights, float weights[5][5])
+{
+    for (int i = 0; i < 25; i++)
+    {
+        weights[i / 5][i % 5] = flattenedWeights[i];
+    }
+}
+
 void gaussFilter(Pixel *image, int width, int height, float weight[5][5])
 {
     Pixel *newImage = (Pixel *)malloc(sizeof(Pixel) * width * height);
@@ -151,10 +270,189 @@ void gaussFilter(Pixel *image, int width, int height, float weight[5][5])
     free(newImage);
 }
 
+void checkError(cl_int err)
+{
+    if (err != CL_SUCCESS)
+        printf("Error with errorcode: %d\n", err);
+}
+
+void printBuildLog(cl_program program, cl_device_id device)
+{
+    cl_int err;
+    char *build_log;
+    size_t build_log_size;
+    // Speichere den Build Log fuer program und device in build_log
+    err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &build_log_size);
+    checkError(err);
+
+    build_log = (char *)malloc(build_log_size);
+
+    err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, build_log_size, build_log, NULL);
+    checkError(err);
+
+    printf("Log:\n%s\n", build_log);
+
+    free(build_log);
+}
+
+void initOpenCL()
+{
+    cl_int err;
+
+    // Speichere 1 Plattform in platform
+    err = clGetPlatformIDs(1, &platform, NULL);
+    checkError(err);
+    printf("platform selected\n");
+
+    // Speichere 1 Device beliebigen Typs in device
+    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, NULL);
+    checkError(err);
+    printf("device selected\n");
+
+    // erzeuge Context fuer das Device device
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    checkError(err);
+    printf("context created\n");
+
+    // erzeuge Command Queue zur Verwaltung von device
+    commandQueue = clCreateCommandQueue(context, device, 0, &err);
+    checkError(err);
+    printf("commandQueue created\n");
+}
+
+void makeKernel()
+{
+    cl_int err;
+    // Laenge des Kernel Quellcodes
+    size_t sourceLength = strlen(kernelSource);
+    cl_program program;
+    // Ein Programm aus dem Kernel Quellcode wird erzeugt
+    program = clCreateProgramWithSource(context, 1, &kernelSource, &sourceLength, &err);
+    checkError(err);
+    printf("program created\n");
+    // Das Programm wird fuer alle Devices des Contextes gebaut
+    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if (err != CL_SUCCESS)
+        printBuildLog(program, device);
+    else
+        printf("program build successfully\n");
+    kernel = clCreateKernel(program, "gaussFilter", &err);
+    checkError(err);
+    printf("kernel created\n");
+}
+
+Pixel *gaussFilterOpenCL(Pixel *image, int width, int height, float weight[5][5])
+{
+    initOpenCL();
+    makeKernel();
+    cl_int err;
+
+    // flattening input data
+    float *flatWeights = flattenWeights(weight);
+    unsigned char *flatRValues = flattenRValues(image, width, height);
+    unsigned char *flatGValues = flattenGValues(image, width, height);
+    unsigned char *flatBValues = flattenBValues(image, width, height);
+
+    // create output data
+    unsigned char *outRValues = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
+    unsigned char *outGValues = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
+    unsigned char *outBValues = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
+
+    // do some memory allocation on the device
+    cl_mem inputRValues = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char) * width * height, NULL, &err);
+    checkError(err);
+    std::cout << "inputRValues created" << std::endl;
+
+    cl_mem inputGValues = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char) * width * height, NULL, &err);
+    checkError(err);
+    std::cout << "inputGValues created" << std::endl;
+
+    cl_mem inputBValues = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char) * width * height, NULL, &err);
+    checkError(err);
+    std::cout << "inputBValues created" << std::endl;
+
+    cl_mem inputWeights = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * 25, NULL, &err);
+    checkError(err);
+    std::cout << "inputWeights created" << std::endl;
+
+    cl_mem outputRValues = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(unsigned char) * width * height, NULL, &err);
+    checkError(err);
+    std::cout << "outputRValues created" << std::endl;
+
+    cl_mem outputGValues = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(unsigned char) * width * height, NULL, &err);
+    checkError(err);
+    std::cout << "outputGValues created" << std::endl;
+
+    cl_mem outputBValues = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(unsigned char) * width * height, NULL, &err);
+    checkError(err);
+    std::cout << "outputBValues created" << std::endl;
+
+    // set kernel arguments
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputRValues);
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &inputGValues);
+    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &inputBValues);
+    err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &outputRValues);
+    err |= clSetKernelArg(kernel, 4, sizeof(cl_mem), &outputGValues);
+    err |= clSetKernelArg(kernel, 5, sizeof(cl_mem), &outputBValues);
+    err |= clSetKernelArg(kernel, 6, sizeof(cl_mem), &inputWeights);
+    err |= clSetKernelArg(kernel, 7, sizeof(int), &width);
+    err |= clSetKernelArg(kernel, 8, sizeof(int), &height);
+    checkError(err);
+    std::cout << "kernel arguments set" << std::endl;
+
+    // copy input data to device
+    err = clEnqueueWriteBuffer(commandQueue, inputRValues, CL_TRUE, 0, sizeof(unsigned char) * width * height, flatRValues, 0, NULL, NULL);
+    err |= clEnqueueWriteBuffer(commandQueue, inputGValues, CL_TRUE, 0, sizeof(unsigned char) * width * height, flatGValues, 0, NULL, NULL);
+    err |= clEnqueueWriteBuffer(commandQueue, inputBValues, CL_TRUE, 0, sizeof(unsigned char) * width * height, flatBValues, 0, NULL, NULL);
+    err |= clEnqueueWriteBuffer(commandQueue, inputWeights, CL_TRUE, 0, sizeof(float) * 25, flatWeights, 0, NULL, NULL);
+    checkError(err);
+    std::cout << "input data copied to device" << std::endl;
+
+    // execute kernel
+    size_t globalWorkSize[2] = {width, height};
+    err = clEnqueueNDRangeKernel(commandQueue, kernel, 2, NULL, globalWorkSize, NULL, 0, NULL, NULL);
+    checkError(err);
+    std::cout << "kernel executed" << std::endl;
+
+    // copy output data back to host
+    err = clEnqueueReadBuffer(commandQueue, outputRValues, CL_TRUE, 0, sizeof(unsigned char) * width * height, outRValues, 0, NULL, NULL);
+    err |= clEnqueueReadBuffer(commandQueue, outputGValues, CL_TRUE, 0, sizeof(unsigned char) * width * height, outGValues, 0, NULL, NULL);
+    err |= clEnqueueReadBuffer(commandQueue, outputBValues, CL_TRUE, 0, sizeof(unsigned char) * width * height, outBValues, 0, NULL, NULL);
+    checkError(err);
+    std::cout << "output data copied back to host" << std::endl;
+
+    // combine the three channels to one image
+    Pixel *newImage = unflattenRGBValues(outRValues, outGValues, outBValues, width, height); // must be freed by caller
+
+    // free memory
+    free(flatWeights);
+    free(flatRValues);
+    free(flatGValues);
+    free(flatBValues);
+    free(outRValues);
+    free(outGValues);
+    free(outBValues);
+    std::cout << "host memory freed" << std::endl;
+
+    // free device memory
+    err = clReleaseMemObject(inputRValues);
+    err |= clReleaseMemObject(inputGValues);
+    err |= clReleaseMemObject(inputBValues);
+    err |= clReleaseMemObject(inputWeights);
+    err |= clReleaseMemObject(outputRValues);
+    err |= clReleaseMemObject(outputGValues);
+    err |= clReleaseMemObject(outputBValues);
+    checkError(err);
+    std::cout << "device memory freed" << std::endl;
+
+    return newImage;
+}
+
 int main(int argc, char **argv)
 {
     const char *inFilename = (argc > 1) ? argv[1] : "lena.ppm";
-    const char *outFilename = (argc > 2) ? argv[2] : "output.ppm";
+    const char *outFilename_seq = (argc > 2) ? argv[2] : "output_seq.ppm";
+    const char *outFilename_opencl = (argc > 3) ? argv[3] : "output_opencl.ppm";
 
     float weights[5][5];
     calculateWeights(weights);
@@ -165,18 +463,33 @@ int main(int argc, char **argv)
     Pixel *image = readPPM(inFilename, &width, &height);
     std::cout << "Done reading image" << std::endl;
 
-    std::cout << "Applying filter" << std::endl;
-    gaussFilter(image, width, height, weights);
-    std::cout << "Done applying filter" << std::endl;
+    // seq gaussFilter updates the image in place so we will call the kernel first
+    std::cout << "Applying opencl filter" << std::endl;
+    Pixel *newImage = gaussFilterOpenCL(image, width, height, weights);
+    std::cout << "Done applying opencl filter" << std::endl;
 
-    std::cout << "Writing image" << outFilename << std::endl;
-    writePPM(image, outFilename, width, height);
-    std::cout << "Done writing image" << std::endl;
+    std::cout << "Writing opencl image " << outFilename_opencl << std::endl;
+    writePPM(newImage, outFilename_opencl, width, height);
+    std::cout << "Done writing opencl image" << std::endl;
+
+    std::cout << "Applying seq. filter" << std::endl;
+    gaussFilter(image, width, height, weights);
+    std::cout << "Done applying seq. filter" << std::endl;
+
+    std::cout << "Writing seq. image " << outFilename_seq << std::endl;
+    writePPM(image, outFilename_seq, width, height);
+    std::cout << "Done writing seq. image" << std::endl;
+
+    std::cout << "Comparing images" << std::endl;
+    const float delta = 0.0001;
+    compareImages(image, newImage, width, height, delta);
+    std::cout << "Done comparing images" << std::endl;
 
     std::cout << "Freeing memory" << std::endl;
     free(image); // must be explicitly freed
+    free(newImage);
     std::cout << "ALL done!" << std::endl;
     return 0;
 }
 
-// g++ -o main main.cpp && ./main
+// g++ -o main main.cpp -lOpenCL && ./main
